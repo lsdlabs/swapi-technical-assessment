3be21f921a50eedea23c60445c2162d14002915e
Lauren Small <lauren.small@detroitlabs.com>
2018-10-28 15:25:07 -0400

    Recovered FilmInfo and FilmInfoViewModel

---
+++ StarWars/StarWars/Model/FilmInfoViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FilmInfoViewModel.swift
+//  StarWars
+//
+//  Created by Lauren Small on 10/28/18.
+//  Copyright © 2018 Lauren Small. All rights reserved.
+//
+
+import Foundation
+class FilmInfoViewModel: Codable {
+    
+    var title = ""
+    var characters: [String] = []
+    
+}
