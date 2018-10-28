05a1a01f5e85d83c90f214d31c04e11c4e9e2ddb
Lauren Small <lauren.small@detroitlabs.com>
2018-10-28 11:53:47 -0400

    created empty xcode project

---
+++ StarWars/StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Lauren Small on 10/28/18.
+//  Copyright © 2018 Lauren Small. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
