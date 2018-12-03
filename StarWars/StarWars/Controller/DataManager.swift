8e1699e3479722213087b1598b214798a53b321a
Lauren Small <lauren.small@detroitlabs.com>
2018-12-03 09:26:32 -0500

    Removed fetchHomeworld() and fetchSpecies() from CharacterDataClass and placed inside DataManager class

--- StarWars/StarWars/Controller/DataManager.swift
+++ StarWars/StarWars/Controller/DataManager.swift
@@ -16,6 +16,7 @@ class DataManager {
     
     let filmURL = "https://swapi.co/api/films/2/"
     var characterArray = [CharacterData]()
+    let characterDataInstance = CharacterData()
     
     
     func fetchCharacters(completion: @escaping () -> Void ) {
@@ -57,7 +58,90 @@ class DataManager {
         task.resume()
     }
     
+    
+    
+    var homeworld: PlanetData?
+    
+    func fetchHomeworld(completion: @escaping () -> Void) {
+        guard let url = URL(string: characterDataInstance.homeworldURL) else {
+            completion()
+            return
+        }
+        let planetTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do {
+                if let planetData = data {
+                    let planetInfo = try JSONDecoder().decode(PlanetData.self, from: planetData)
+                    print(planetInfo.name)
+                    self.homeworld = planetInfo
+                }
+            } catch {
+                print(error)
+            }
+            completion()
+        }
+        planetTask.resume()
+        
+    }
+    
+    
+    
+    
+    
+    var species: SpeciesData?
+    
+    func fetchSpecies(completion: @escaping () -> Void) {
+        guard let url = URL(string: characterDataInstance.speciesURL[0]) else {
+            completion()
+            return
+        }
+        let speciesTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do {
+                if let speciesData = data {
+                    let speciesInfo = try JSONDecoder().decode(SpeciesData.self, from: speciesData)
+                    print(speciesInfo.name)
+                    self.species = speciesInfo
+                }
+            } catch {
+                print(error)
+            }
+            completion()
+        }
+        speciesTask.resume()
+        
+    }
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct FilmInfo: Codable {
     var characters: [URL]
